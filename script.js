37f22fc1fbb87d3c5bfda26ee5a313054b2c382d
gamerWOT9 <sulzynski.olivier@gmail.com>
2022-07-01 10:41:12 +0200

    BETA V0.15
    Bouton ...plus

--- script.js
+++ script.js
@@ -21,3 +21,20 @@ function openCity(evt, cityName) {
 }
 evt.currentTarget.className += " active";
 }
+
+
+/*
+function compare_lname( a, b )
+  {
+  if ( a.lname.toLowerCase() < b.lname.toLowerCase()){
+    return -1;
+  }
+  if ( a.lname.toLowerCase() > b.lname.toLowerCase()){
+    return 1;
+  }
+  return 0;
+}
+
+students.sort(compare_lname);
+console.log(students)
+*/
